e904dfa9ef09ef5ef2b47bfd968774c12336cebc
Hemant Pratap Singh <shemantpratap983@email.com>
2022-08-27 21:36:43 +0530

    day5

---
+++ furniture.js
@@ -0,0 +1,255 @@
+itemObj=[
+    {
+        imgUrl:"https://images.urbndata.com/is/image/Anthropologie/4130916210042_050_c?$an-category$&fit=constrain&fmt=webp&hei=1046&qlt=80&wid=698",
+        price:"$ 180.00"
+        ,line:"The Marais Printed Chiffon Maxi Dress",
+        color:"9 colors"
+
+
+    },
+    {
+        imgUrl:"https://images.urbndata.com/is/image/Anthropologie/4130916210092_070_b?$an-category$&fit=constrain&fmt=webp&hei=1046&qlt=80&wid=698",
+        price:"$ 160.00",
+        line:"Meeve High-Low Strapless Midi Dress",
+        color:"2 colore"
+    },
+    {
+        imgUrl:"https://images.urbndata.com/is/image/Anthropologie/4130360680045_061_b15?$an-category$&fit=constrain&fmt=webp&hei=1046&qlt=80&wid=698",
+        line:"Bhanuni by Jyoti Patterned Maxi Dress",
+        price:"$288.00"
+        ,color:"2 color"
+
+    },
+    {
+        imgUrl:"https://images.urbndata.com/is/image/Anthropologie/4130600740007_027_b14?$an-category$&fit=constrain&fmt=webp&hei=1046&qlt=80&wid=698",
+        line:"  Guapa One-Shoulder Mini Dress"
+        ,price:"260.00",
+        color:"2 colors"
+        
+         
+    }
+    ,{imgUrl:"https://images.urbndata.com/is/image/Anthropologie/4130370060072_044_b?$an-category$&fit=constrain&fmt=webp&hei=1046&qlt=80&wid=698",
+    line:"The Somerest Maxi Dress",
+    price:"$170",
+    color:"9 colors"
+
+    }
+    ,{imgUrl:"https://images.urbndata.com/is/image/Anthropologie/4130652010057_266_b14?$an-category$&fit=constrain&fmt=webp&hei=1046&qlt=80&wid=698",
+    line:"By Anthropologie Puff-Sleeve Organza Midi Dress",
+    price:"$260",
+    color:""
+
+    }
+    ,{imgUrl:"https://images.urbndata.com/is/image/Anthropologie/4130370060007_085_b?$an-category$&fit=constrain&fmt=webp&hei=1046&qlt=80&wid=698",
+    line:"Maeve Cassandra Maxi Dress",
+    price:"$160",
+    color:"5 colors"
+
+    }
+    ,{imgUrl:"https://images.urbndata.com/is/image/Anthropologie/4130647160145_027_b?$an-category$&fit=constrain&fmt=webp&hei=1046&qlt=80&wid=698",
+    line:"By Anthropologie Long-Sleeve Mini Dress",
+    price:"$160",
+    color:""
+
+    }
+    ,{imgUrl:"https://images.urbndata.com/is/image/Anthropologie/4130916210029_009_b?$an-category$&fit=constrain&fmt=webp&hei=1046&qlt=80&wid=698",
+    line:"",
+    price:"By Anthropologie The Peregrine Midi ",
+    color:""
+
+    }
+    ,{imgUrl:"https://images.urbndata.com/is/image/Anthropologie/4130615360010_066_b14?$an-category$&fit=constrain&fmt=webp&hei=1046&qlt=80&wid=698",
+    line:"Dhruv Kapoor Smocked Cerise Dress",
+    price:"$198",
+    color:"$198.00"
+
+    }
+    ,{imgUrl:"https://images.urbndata.com/is/image/Anthropologie/4130344600124_009_b?$an-category$&fit=constrain&fmt=webp&hei=1046&qlt=80&wid=698",
+    line:"Forever That Girl Vivienne Maxi Dres",
+    price:"$198.00",
+    color:""
+
+    }
+    ,{imgUrl:"https://images.urbndata.com/is/image/Anthropologie/4130646420009_063_b?$an-category$&fit=constrain&fmt=webp&hei=1046&qlt=80&wid=698",
+    line:"THe Somereste Maxi Dress",
+    price:"$168",
+    color:"8 colors"
+
+    }
+    ,{imgUrl:"https://images.urbndata.com/is/image/Anthropologie/4130344600159_046_b?$an-category$&fit=constrain&fmt=webp&hei=1046&qlt=80&wid=698",
+    line:"Temperly Printed V-Neck Dress",
+    price:"$158.00",
+    color:""
+
+    }
+    ,{imgUrl:"https://images.urbndata.com/is/image/Anthropologie/4130360680073_020_b?$an-category$&fit=constrain&fmt=webp&hei=1046&qlt=80&wid=698",
+    line:"Bhanuni by Joyti Crichet Fringe Dress",
+    price:"$230",
+    color:""
+
+    }
+    ,{imgUrl:"",
+    line:"",
+    price:"",
+    color:""
+
+    }
+    ,{imgUrl:"",
+    line:"",
+    price:"",
+    color:""
+
+    }
+    ,{imgUrl:"",
+    line:"",
+    price:"",
+    color:""
+
+    }
+    ,{imgUrl:"",
+    line:"",
+    price:"",
+    color:""
+
+    }
+    ,{imgUrl:"",
+    line:"",
+    price:"",
+    color:""
+
+    }
+    ,{imgUrl:"",
+    line:"",
+    price:"",
+    color:""
+
+    }
+    ,{imgUrl:"",
+    line:"",
+    price:"",
+    color:""
+
+    }
+    ,{imgUrl:"",
+    line:"",
+    price:"",
+    color:""
+
+    }
+    ,{imgUrl:"",
+    line:"",
+    price:"",
+    color:""
+
+    }
+    ,{imgUrl:"",
+    line:"",
+    price:"",
+    color:""
+
+    }
+    ,{imgUrl:"",
+    line:"",
+    price:"",
+    color:""
+
+    }
+    ,{imgUrl:"",
+    line:"",
+    price:"",
+    color:""
+
+    }
+    ,{imgUrl:"",
+    line:"",
+    price:"",
+    color:""
+
+    }
+    ,{imgUrl:"",
+    line:"",
+    price:"",
+    color:""
+
+    } ,{imgUrl:"",
+    line:"",
+    price:"",
+    color:""
+
+    }
+    ,{imgUrl:"",
+    line:"",
+    price:"",
+    color:""
+
+    }
+    ,{imgUrl:"",
+    line:"",
+    price:"",
+    color:""
+
+    }
+    ,{imgUrl:"",
+    line:"",
+    price:"",
+    color:""
+
+    }
+    ,{imgUrl:"",
+    line:"",
+    price:"",
+    color:""
+
+    }
+]
+
+
+arr=[]
+
+itemObj.map(function(el,i){
+
+    row=document.createElement("div")
+    a=document.createElement("a")
+    // Create anchor element.
+    var a = document.createElement('a'); 
+                  
+    // Create the text node for anchor element.
+   
+      
+    // Set the title.
+   
+      
+    // Set the href property.
+    a.href = "show.htm"; 
+      
+    // Append the anchor element to the body.
+    document.body.appendChild(a); 
+    
+    divimg=document.createElement("div")
+
+
+
+    product_img=document.createElement("img")
+    product_img.setAttribute("src", el.imgUrl)
+
+  price=document.createElement("p")
+  price.innerText=el.price
+
+  line=document.createElement("p")
+  line.innerText=el.line
+
+  color=document.createElement("p")
+  color.innerText=el.colo
+
+
+
+  row.addEventListener("click",function(){
+    arr.push(el)
+    localStorage.setItem("product_details",JSON.stringify(arr))
+  })
+row.append(product_img,line,price,color)
+a.append(row)
+
+document.querySelector("#item").append(a)
+
+})
